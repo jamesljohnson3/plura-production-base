447859eb5ef76ff6f541947d8d7eb8a33b763d73
Admin <support@unlimitpotential.com>
2024-02-03 07:02:19 -0500

    first commit

---
+++ app/layout.tsx
@@ -0,0 +1,45 @@
+import type { Metadata } from 'next'
+import { DM_Sans } from 'next/font/google'
+import './globals.css'
+import { ClerkProvider } from '@clerk/nextjs'
+import { dark } from '@clerk/themes'
+import { ThemeProvider } from '@/providers/theme-provider'
+import ModalProvider from '@/providers/modal-provider'
+import { Toaster } from '@/components/ui/toaster'
+import { Toaster as SonnarToaster } from '@/components/ui/sonner'
+import siteMetadata from "@/app/utils/siteMetaData";
+
+const font = DM_Sans({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: siteMetadata.title,
+  description: siteMetadata.description,
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html
+      lang="en"
+      suppressHydrationWarning
+    >
+      <body className={font.className}>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <ModalProvider>
+            {children}
+            <Toaster />
+            <SonnarToaster position="bottom-left" />
+          </ModalProvider>
+        </ThemeProvider>
+      </body>
+    </html>
+  )
+}
---
+++ app/site/page.tsx
@@ -0,0 +1,133 @@
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card'
+import { pricingCards } from '@/lib/constants'
+import { stripe } from '@/lib/stripe'
+import clsx from 'clsx'
+import { Check } from 'lucide-react'
+import Image from 'next/image'
+import Link from 'next/link'
+
+export default async function Home() {
+  const prices = await stripe.prices.list({
+    product: process.env.NEXT_PLURA_PRODUCT_ID,
+    active: true,
+  })
+
+  return (
+    <>
+      <section className="h-full w-full md:pt-44 mt-[-70px] relative flex items-center justify-center flex-col ">
+ 
+        <div className="flex  justify-center gap-4 flex-wrap mt-6">
+          {prices.data.map((card) => (
+            //WIP: Wire up free product from stripe
+            <Card
+              key={card.nickname}
+              className={clsx('w-[300px] flex flex-col justify-between', {
+                'border-2 border-primary': card.nickname === 'Unlimited Saas',
+              })}
+            >
+              <CardHeader>
+                <CardTitle
+                  className={clsx('', {
+                    'text-muted-foreground': card.nickname !== 'Unlimited Saas',
+                  })}
+                >
+                  {card.nickname}
+                </CardTitle>
+                <CardDescription>
+                  {
+                    pricingCards.find((c) => c.title === card.nickname)
+                      ?.description
+                  }
+                </CardDescription>
+              </CardHeader>
+              <CardContent>
+                <span className="text-4xl font-bold">
+                  {card.unit_amount && card.unit_amount / 100}
+                </span>
+                <span className="text-muted-foreground">
+                  <span>/ {card.recurring?.interval}</span>
+                </span>
+              </CardContent>
+              <CardFooter className="flex flex-col items-start gap-4">
+                <div>
+                  {pricingCards
+                    .find((c) => c.title === card.nickname)
+                    ?.features.map((feature) => (
+                      <div
+                        key={feature}
+                        className="flex gap-2"
+                      >
+                        <Check />
+                        <p>{feature}</p>
+                      </div>
+                    ))}
+                </div>
+                <Link
+                  href={`/agency?plan=${card.id}`}
+                  className={clsx(
+                    'w-full text-center bg-primary p-2 rounded-md',
+                    {
+                      '!bg-muted-foreground':
+                        card.nickname !== 'Unlimited Saas',
+                    }
+                  )}
+                >
+                  Get Started
+                </Link>
+              </CardFooter>
+            </Card>
+          ))}
+          <Card className={clsx('w-[300px] flex flex-col justify-between')}>
+            <CardHeader>
+              <CardTitle
+                className={clsx({
+                  'text-muted-foreground': true,
+                })}
+              >
+                {pricingCards[0].title}
+              </CardTitle>
+              <CardDescription>{pricingCards[0].description}</CardDescription>
+            </CardHeader>
+            <CardContent>
+              <span className="text-4xl font-bold">$0</span>
+              <span>/ month</span>
+            </CardContent>
+            <CardFooter className="flex flex-col  items-start gap-4 ">
+              <div>
+                {pricingCards
+                  .find((c) => c.title === 'Starter')
+                  ?.features.map((feature) => (
+                    <div
+                      key={feature}
+                      className="flex gap-2"
+                    >
+                      <Check />
+                      <p>{feature}</p>
+                    </div>
+                  ))}
+              </div>
+              <Link
+                href="/agency"
+                className={clsx(
+                  'w-full text-center bg-primary p-2 rounded-md',
+                  {
+                    '!bg-muted-foreground': true,
+                  }
+                )}
+              >
+                Get Started
+              </Link>
+            </CardFooter>
+          </Card>
+        </div>
+      </section>
+    </>
+  )
+}
