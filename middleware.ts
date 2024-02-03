ab0b43f8201e536e86c6dec6c23308c0fe413314
Admin <support@unlimitpotential.com>
2024-02-03 10:40:48 -0500

    j

--- middleware.ts
+++ middleware.ts
@@ -29,9 +29,7 @@ export default authMiddleware({
       )
     }
 
-    if (url.pathname === '/sign-in' || url.pathname === '/sign-up') {
-      return NextResponse.redirect(new URL(`/agency/sign-in`, req.url))
-    }
+   
 
     if (
       url.pathname === '/' ||
